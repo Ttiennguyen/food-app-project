959013f9ab6db0db432d565cf98c05af5190facf
Ttiennguyen <trantiennguyen39@gmail.com>
2023-11-21 11:26:21 +0700

    Home UI

--- App.js
+++ App.js
@@ -1,17 +1,12 @@
-import React, { Component } from 'react';
-import Main from './components/MainComponent';
-// redux
-import { Provider } from 'react-redux';
-import { ConfigureStore } from './redux/ConfigureStore';
-const store = ConfigureStore();
+import React from 'react';
+import { StatusBar } from 'expo-status-bar';
+import {StyleSheet,Text, View} from 'react-native';
+import Navigation from './navigation';
 
-class App extends Component {
-  render() {
-    return (
-      <Provider store={store}>
-      <Main />
-    </Provider>
-    );
-  }
-}
-export default App;
+import tw from 'twrnc'
+export default function App(){
+  return(
+    
+    <Navigation />
+  )
+} 
